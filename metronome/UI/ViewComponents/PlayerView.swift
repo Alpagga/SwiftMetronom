000b260a726b7e291f0df03d963564bc7636593d
lukasschmid <lukas.schmid.01@icloud.com>
2024-02-28 17:32:42 +0100

    working fine

---
+++ metronome/UI/ViewComponents/PlayerView.swift
@@ -0,0 +1,9 @@
+//
+//  PlayerView.swift
+//  metronome
+//
+//  Created by Lukas Schmid on 28.02.24.
+//  Copyright © 2024 Jan Kříž. All rights reserved.
+//
+
+import Foundation
---
+++ metronome/UI/ViewComponents/UIPlyayerView.swift
@@ -0,0 +1,9 @@
+//
+//  UIPlyayerView.swift
+//  metronome
+//
+//  Created by Lukas Schmid on 28.02.24.
+//  Copyright © 2024 Jan Kříž. All rights reserved.
+//
+
+import Foundation
